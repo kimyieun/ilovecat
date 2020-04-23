22b81337617bd340f47bcbfd4a5e6078fdb633be
yieun12.kim <yieun12.kim@samsung.com>
2020-04-24 08:51:47 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ilovecat
+# 프로그래머스 2020 Dev-Matching 웹 프론트엔드 과제 복기
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import SearchBar from "./components/SearchBar.js";
+import SearchResult from "./components/SearchResult.js";
+import { api } from "./api/theCatAPI.js";
+
+export default class App {
+  constructor() {
+    console.log("App is created!");
+
+    const body = document.body;
+
+    const searchBar = new SearchBar(body);
+    const searchResult = new SearchResult(body, []);
+
+    const data = api.fetchImage("no");
+    const data2 = api.fetchImageAll();
+
+    data.then((cats) => {
+      console.log(cats);
+    });
+
+    data2.then((cats) => {
+      console.log(cats);
+    });
+  }
+}
